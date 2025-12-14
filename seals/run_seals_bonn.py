76f5bd9f02315b682d3bbba30e1020b971b14696
Justin Johnson <jandrewjohnson@gmail.com>
2025-12-14 11:18:22 -0600

    .

--- seals/run_seals_bonn.py
+++ seals/run_seals_bonn.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     # files that already exist.
     p.user_dir = os.path.expanduser('~')
     p.extra_dirs = ['Files', 'seals', 'projects']
-    p.project_name = 'bonn_esm_cgebox_seals'
+    p.project_name = 'bonn_devstack'
     # p.project_name = p.project_name + '_' + hb.pretty_time() # If don't you want to recreate everything each time, comment out this line.
 
     # Based on the paths above, set the project_dir. All files will be created in this directory.
